431eccb17a58a69222826a92f4d509a30e34ca92
Seth Itow <thedingwing@gmail.com>
2015-05-14 11:11:47 -0700

    Fixed project

---
+++ 2015_Canyon_Rescue_Robotics/.ino.cpp
@@ -0,0 +1,26 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-05-14 11:09:04
+
+#include "Arduino.h"
+#include "config.h"
+#include <SPI.h>
+#include <WiFi.h>
+#include <SoftwareSerial.h>
+#include <HardwareSerial.h>
+#include <Adafruit_GPS.h>
+#include <Servo.h>
+#include <PID_v1.h>
+#include <SD.h>
+#include <Adafruit_VC0706.h>
+#include "RelativePositionController.h"
+#include "FlightController.h"
+#include "VictimLoggerController.h"
+void setup() ;
+SIGNAL(TIMER0_COMPA_vect) ;
+void useInterrupt(boolean v) ;
+void loop() ;
+
+
+#include "2015_Canyon_Rescue_Robotics.ino"
